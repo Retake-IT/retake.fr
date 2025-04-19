52017c81a3a4a1621237cea4669a5e226c26d40b
l-nmch <leo@nonnenmacher-logel.fr>
2025-04-19 14:55:43 +0200

    chore: Added new articles

--- src/components/products.js
+++ src/components/products.js
@@ -1,22 +1,46 @@
 const Products = [
-    {
-      imageUrl: "https://img.leboncoin.fr/api/v1/lbcpb1/images/02/60/84/026084690ccdb649efa30f07c2228736a8e6f5a7.jpg?rule=classified-1200x800-webp",
-      title: "Serveur Workstation HP Z440",
-      description: "Serveur / Workstation / Ordinateur de CAO",
-      url: "https://www.leboncoin.fr/ad/ordinateurs/2851218069",
-    },
-    {
-      imageUrl: "https://img.leboncoin.fr/api/v1/lbcpb1/images/de/4e/73/de4e73c704bb22a74493b93bd6d07e4a522ab3ae.jpg?rule=classified-1200x800-webp",
-      title: "Mini ordinateur Lenovo tiny m72e",
-      description: "Mini PC Lenovo tiny ThinkCenter m72e",
-      url: "https://www.leboncoin.fr/ad/ordinateurs/2859959432",
-    },
-    {
-      imageUrl: "https://img.leboncoin.fr/api/v1/lbcpb1/images/78/dc/ce/78dcce7f5f4c98298a4446af25bc2b4358ecac5f.jpg?rule=classified-1200x800-webp",
-      title: "Imprimante HP LaserJet Pro 400 m401dn",
-      description: "Imprimante Laser HP m401dn",
-      url: "https://www.leboncoin.fr/ad/accessoires_informatique/2889667257",
-    },
-  ];
+  {
+    imageUrl: "https://img.leboncoin.fr/api/v1/lbcpb1/images/02/60/84/026084690ccdb649efa30f07c2228736a8e6f5a7.jpg?rule=classified-1200x800-webp",
+    title: "Serveur Workstation HP Z440",
+    description: "Serveur / Workstation / Ordinateur de CAO",
+    url: "https://www.leboncoin.fr/ad/ordinateurs/2851218069",
+  },
+  {
+    imageUrl: "https://img.leboncoin.fr/api/v1/lbcpb1/images/ef/f3/66/eff3667a2b06a50c1b2bd845692f70b515483238.jpg?rule=classified-1200x800-webp",
+    title: "Mini Ordinateur HP ProDesk 600 G2",
+    description: "Mini PC HP ProDesk 600 G2",
+    url: "https://www.leboncoin.fr/ad/ordinateurs/2943332797",
+  },
+  {
+    imageUrl: "https://img.leboncoin.fr/api/v1/lbcpb1/images/78/dc/ce/78dcce7f5f4c98298a4446af25bc2b4358ecac5f.jpg?rule=classified-1200x800-webp",
+    title: "Imprimante HP LaserJet Pro 400 m401dn",
+    description: "Imprimante Laser HP m401dn",
+    url: "https://www.leboncoin.fr/ad/accessoires_informatique/2889667257",
+  },
+  {
+    imageUrl: "https://img.leboncoin.fr/api/v1/lbcpb1/images/41/af/8a/41af8a10378cd8902329f36175cc06f1389b865d.jpg?rule=classified-1200x800-webp",
+    title: "Siwtch Cisco Catalyst 3850 24 Ports",
+    description: "Commutateur réseau Cisco Catalyst 24 Ports",
+    url: "https://www.leboncoin.fr/ad/accessoires_informatique/2974074730",
+  },
+  {
+    imageUrl: "https://img.leboncoin.fr/api/v1/lbcpb1/images/44/b5/83/44b583c45c2efb7a31bbcdbb4faad434b277b7cf.jpg?rule=classified-1200x800-webp",
+    title: "Siwtch Cisco Catalyst 3850 48 Ports",
+    description: "Commutateur réseau Cisco Catalyst 48 Ports",
+    url: "https://www.leboncoin.fr/ad/accessoires_informatique/2974071676",
+  },
+  {
+    imageUrl: "https://img.leboncoin.fr/api/v1/lbcpb1/images/9c/12/fb/9c12fb37b9de1db30b88d295ff23e55f54882308.jpg?rule=classified-1200x800-webp",
+    title: "Siwtch Cisco Catalyst 3850 48 Ports POE+",
+    description: "Commutateur réseau Cisco Catalyst 48 Ports POE+",
+    url: "https://www.leboncoin.fr/ad/accessoires_informatique/2974071106",
+  },
+  {
+    imageUrl: "https://img.leboncoin.fr/api/v1/lbcpb1/images/6d/df/6d/6ddf6dc0b9264d94c4110f8925e6779d4518ba9d.jpg?rule=classified-1200x800-webp",
+    title: "Siwtch Cisco Catalyst 3850 48 Ports UPOE",
+    description: "Commutateur réseau Cisco Catalyst 48 Ports UPOE",
+    url: "https://www.leboncoin.fr/ad/accessoires_informatique/2974068794",
+  },
+];
 
 export default Products;
