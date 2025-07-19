b1d398081c914e081eade5b27d15e177d23189e0
l-nmch <leo@nonnenmacher-logel.fr>
2025-07-20 00:57:09 +0200

    feat(products) Added new products

--- src/components/products.js
+++ src/components/products.js
@@ -6,12 +6,6 @@ const Products = [
     url: "https://www.leboncoin.fr/ad/ordinateurs/2943332797",
   },
   {
-    imageUrl: "https://img.leboncoin.fr/api/v1/lbcpb1/images/78/dc/ce/78dcce7f5f4c98298a4446af25bc2b4358ecac5f.jpg?rule=classified-1200x800-webp",
-    title: "Imprimante HP LaserJet Pro 400 m401dn",
-    description: "Imprimante Laser HP m401dn",
-    url: "https://www.leboncoin.fr/ad/accessoires_informatique/2889667257",
-  },
-  {
     imageUrl: "https://img.leboncoin.fr/api/v1/lbcpb1/images/41/af/8a/41af8a10378cd8902329f36175cc06f1389b865d.jpg?rule=classified-1200x800-webp",
     title: "Switch Cisco Catalyst 3850 24 Ports",
     description: "Commutateur réseau Cisco Catalyst 24 Ports",
@@ -34,6 +28,54 @@ const Products = [
     title: "Switch Cisco Catalyst 3850 48 Ports UPOE",
     description: "Commutateur réseau Cisco Catalyst 48 Ports UPOE",
     url: "https://www.leboncoin.fr/ad/accessoires_informatique/2974068794",
+  },
+  {
+    imageUrl: "https://img.leboncoin.fr/api/v1/lbcpb1/images/75/67/7d/75677d3d745cd3cb599738075f050ad92101779f.jpg?rule=classified-1200x800-webp",
+    title: "Disque Dur 3.5\" 300 Go SAS 10K – Seagate",
+    description: "HDD SAS 3.5 pouce 300Go Seagate",
+    url: "https://www.leboncoin.fr/ad/accessoires_informatique/3024836169",
+  },
+  {
+    imageUrl: "https://img.leboncoin.fr/api/v1/lbcpb1/images/e7/8c/ab/e78cab4842894c9d9b910147a0eb060f41ec4b81.jpg?rule=classified-1200x800-webp",
+    title: "Disque Dur 3.5\" 300 Go SAS 15K – Seagate",
+    description: "HDD SAS 3.5 pouce 300Go Seagate",
+    url: "https://www.leboncoin.fr/ad/accessoires_informatique/3024835819",
+  },
+  {
+    imageUrl: "https://img.leboncoin.fr/api/v1/lbcpb1/images/b0/26/66/b026669397feb9bfb56e689e2cfe68051816e2e9.jpg?rule=classified-1200x800-webp",
+    title: "Disque Dur 3.5\" 3 To SATA 5400 tr/min – Western Digital",
+    description: "HDD SATA 3.5 pouce 3To Western Digital",
+    url: "https://www.leboncoin.fr/ad/accessoires_informatique/3024834868",
+  },
+  {
+    imageUrl: "https://img.leboncoin.fr/api/v1/lbcpb1/images/f1/f5/ff/f1f5ff1d120531eb94282420d4f03e6be83d960a.jpg?rule=classified-1200x800-webp",
+    title: "Disque Dur 3.5\" 300 Go SAS 15.6K – Hitachi",
+    description: "HDD SAS 3.5 pouce 300Go Hitachi",
+    url: "https://www.leboncoin.fr/ad/accessoires_informatique/3024820780",
+  },
+  {
+    imageUrl: "https://img.leboncoin.fr/api/v1/lbcpb1/images/2f/4d/8d/2f4d8d21373105ae6e8d0a86a74c8a64dde83f41.jpg?rule=classified-1200x800-webp",
+    title: "Disque Dur 3.5\" 146 Go SAS 15K – Fujitsu",
+    description: "HDD SAS 3.5 pouce 146Go Fujitsu",
+    url: "https://www.leboncoin.fr/ad/accessoires_informatique/3024819738",
+  },
+  {
+    imageUrl: "https://img.leboncoin.fr/api/v1/lbcpb1/images/e0/43/e4/e043e4a3284f2d6af6aa36f1556bbb12bb42d8c5.jpg?rule=classified-1200x800-webp",
+    title: "Disque Dur 3.5\" 73 Go SAS 15.5K – Seagate",
+    description: "HDD SAS 3.5 pouce 73Go Seagate",
+    url: "https://www.leboncoin.fr/ad/accessoires_informatique/3024818016",
+  },
+  {
+    imageUrl: "https://img.leboncoin.fr/api/v1/lbcpb1/images/gh/c6/78/ghc6784503d4152af04b8fdd617f6a0070c2ce8c.jpg?rule=classified-1200x800-webp",
+    title: "Disque Dur 3.5\" 2 To SATA - Western Digital",
+    description: "HDD SATA 3.5 pouce 2To Western Digital",
+    url: "https://www.leboncoin.fr/ad/accessoires_informatique/3024805337",
+  },
+  {
+    imageUrl: "https://img.leboncoin.fr/api/v1/lbcpb1/images/b5/0d/4f/b50d4fff93401d4d767fa7e121bb0b7dba4ea2bd.jpg?rule=classified-1200x800-webp",
+    title: "Disque Dur 3.5\" 2 To SATA 7200 tr/min – Seagate",
+    description: "HDD SAS 3.5 pouce 2To Seagate",
+    url: "https://www.leboncoin.fr/ad/accessoires_informatique/3024821935",
   },
 ];
 
